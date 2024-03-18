e3b9d0ca4897425661b07069f5393fc09de83ca2
Kartik Sharma <99817334+Kartikay45@users.noreply.github.com>
2024-03-18 09:12:14 +0530

    Update crop_model.py

--- KrishiVeer_Crop-Recommendation_System/crop_model.py
+++ KrishiVeer_Crop-Recommendation_System/crop_model.py
@@ -17,12 +17,12 @@ y = df['label']
 labels = df['label']
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
 
 from sklearn.tree import DecisionTreeClassifier
 
-DecisionTree = DecisionTreeClassifier(criterion="entropy",random_state=2,max_depth=5)
+DecisionTree = DecisionTreeClassifier(criterion="entropy",random_state=1,max_depth=0)
 
 
 
